131dd2f07818f2ef985a7952d31f711324065522
Tom Thorogood <tomthorogood@outlook.com>
2021-11-20 23:21:45 +0000

    Added `completions` subcomand

---
+++ src/cli/completions.rs
@@ -0,0 +1,27 @@
+use super::opt;
+use clap::IntoApp;
+use clap_generate::Shell;
+use std::path::PathBuf;
+
+pub fn completions(output: Option<PathBuf>, shell: opt::Shell) {
+    let mut app = opt::Opt::into_app();
+
+    let shell = match shell {
+        opt::Shell::Bash => Shell::Bash,
+        opt::Shell::PowerShell => Shell::PowerShell,
+    };
+
+    match output
+        .map(|path| {
+            std::fs::OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open(path)
+                .ok()
+        })
+        .flatten()
+    {
+        Some(mut file) => clap_generate::generate(shell, &mut app, "cipher", &mut file),
+        None => clap_generate::generate(shell, &mut app, "cipher", &mut std::io::stdout()),
+    };
+}
