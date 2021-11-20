ae6790105ae34bc98daf5cdb9e259eb3610b746e
Tom Thorogood <tomthorogood@outlook.com>
2021-11-20 23:59:01 +0000

    Set up solve subcommand

--- src/cli/solve.rs
+++ src/cli/solve.rs
@@ -1,5 +1,21 @@
-use crate::{CipherConfig, SolveOpt};
+use crate::{util, CipherConfig, Opt};
 
-pub fn solve(cfg: &CipherConfig, solve_opt: SolveOpt) -> anyhow::Result<()> {
-    Ok(())
+pub fn solve(cfg: &CipherConfig, solve_opt: Opt) -> anyhow::Result<()> {
+    match solve_opt {
+        #[allow(unused)]
+        Opt::Solve {
+            cipher,
+            crib,
+            crib_pos,
+            show_key,
+            no_plain,
+            lang,
+            text,
+        } => {
+            let text = util::unwrap_or_stdin(text)?;
+
+            Ok(())
+        }
+        _ => unreachable!(),
+    }
 }
