d00d9056d8bfc6501bc05d28476c751be1f72596
Tom Thorogood <tomthorogood@outlook.com>
2021-11-20 23:22:31 +0000

    Helper method for reading stdin

--- src/util.rs
+++ src/util.rs
@@ -5,3 +5,10 @@ pub fn stdin_to_string() -> anyhow::Result<String> {
     io::stdin().lock().read_to_string(&mut buf)?;
     Ok(buf)
 }
+
+pub fn unwrap_or_stdin(text: Option<String>) -> anyhow::Result<String> {
+    match text {
+        Some(text) => Ok(text),
+        None => stdin_to_string(),
+    }
+}
